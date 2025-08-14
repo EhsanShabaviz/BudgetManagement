68a40c91755b75daa2edc5399e952bbab8820e14
Ehsan Shabaviz <e.shabaviz@gmail.com>
2025-08-14 16:57:58 +0330

    feat(dto): add ImportBudgetResultDto for Excel import use case results

---
+++ BudgetManagement.Application/DTOs/Budget/ImportBudgetResultDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManagement.Application.DTOs.Budget
+{
+    public class ImportBudgetResultDto
+    {
+        public int InsertedCount { get; set; }
+        public int UpdatedCount { get; set; }
+        public List<string> Errors { get; set; } = new();
+
+        public ImportBudgetResultDto()
+        {
+        }
+
+        public ImportBudgetResultDto(int insertedCount, int updatedCount, List<string>? errors = null)
+        {
+            InsertedCount = insertedCount;
+            UpdatedCount = updatedCount;
+            Errors = errors ?? new List<string>();
+        }
+    }
+}
+
